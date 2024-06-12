a01743fb8e9f7946993a8536b0b8a1ae43e3cb06
kleytonsolinho <kleytonsolinho@gmail.com>
2024-06-12 16:19:22 -0300

    feat: Add server2 main.go file with HTTP server configuration

---
+++ server2/cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/kleytonsolinho/golang-opentelemetry-zipkin/server2/internal/infra/webserver/handlers"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/cep/{cep}", handlers.GetCepHandler)
+
+	http.ListenAndServe(":8081", r)
+}
